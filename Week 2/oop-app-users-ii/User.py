0f32a14a314e809d729515232d3e4002bacbece9
nhungbi <105992987+nhungbi@users.noreply.github.com>
2022-06-21 09:47:44 -0500

    Add files via upload

---
+++ Week 2/oop-app-users-ii/User.py
@@ -0,0 +1,34 @@
+class User:
+    posts = {}
+
+    def __init__(self, name, email_address, driver_lisense):
+        self.name = name
+        self.email_address = email_address
+        self.driver_liscense = driver_lisense
+        self.user_posts = []
+    
+    def create_post(self, post):
+        self.user_posts.append(post)
+        User.posts[self.name] = self.user_posts
+    
+    def delete_post(self, post_to_delete):
+        self.user_posts.remove(post_to_delete)
+        User.posts[self.name] = self.user_posts
+
+
+    def __str__(self): 
+        return f'User: {self.name} with an email address of {self.email_address} has liscense {self.driver_liscense}. Posts includes {self.user_posts}.'
+
+
+
+anna = User('Anna', 'annahhello@gmail.com', 78743987957)
+anna.create_post('helloworld')
+anna.create_post('goodbye')
+anna.delete_post('goodbye')
+print(anna)
+
+john = User('John', 'johhnyy@gmail.com', 7908098098)
+john.create_post('evening')
+print(john)
+
+print(User.posts)
